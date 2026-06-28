3ffd4eac6c4d985f7dfa31157f6f78260ba952c0
Antonio Marasescu-Duran <antonio.marasescuduran@gmail.com>
2026-06-28 16:05:55 +0300

    chore: some more setup

--- chronicle-ui/src/app/app.routes.ts
+++ chronicle-ui/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { AppRoutes } from './core/const/app-routes.constants';
 
 export const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: AppRoutes.Home.Base
+  },
+  {
     path: AppRoutes.Home.Base,
     loadChildren: () => import('./features/home/home.routes').then(m => m.HOME_ROUTES)
   }
